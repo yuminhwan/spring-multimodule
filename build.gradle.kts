fd1cfb085b20b6eeb6caa7cd4b1e632927d01e9d
yuminhwan <ymh7532@icloud.com>
2023-09-10 21:45:20 +0900

    chore: 버전관리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,21 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.22"
-    kotlin("plugin.spring") version "1.8.22" apply false
-    id("org.springframework.boot") version "3.1.3" apply false
-    id("io.spring.dependency-management") version "1.1.3" apply false
+    kotlin("jvm")
+    kotlin("plugin.spring") apply false
+    id("org.springframework.boot") apply false
+    id("io.spring.dependency-management") apply false
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
 }
 
+val projectGroup: String by project
+val applicationVersion: String by project
 allprojects {
-    group = "com.example"
-    version = "0.0.1-SNAPSHOT"
+    group = projectGroup
+    version = applicationVersion
 
     repositories {
         mavenCentral()
